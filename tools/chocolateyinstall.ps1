1773e66b6eca1d4c3cd715e0fade10a1dfaddda3
Ariel Jannai <arieljannai@gmail.com>
2016-06-19 23:08:25 +0300

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿
+$ErrorActionPreference = "Stop";
+$AppDataNVM = "$env:APPDATA\nvm";
+$NodeJsInstall = "C:\Program Files\nodejs";
+$OsBits = Get-ProcessorBits;
+$NvmSettings = "$AppDataNVM\settings.txt";
+$ZipUrl = "https://github.com/coreybutler/nvm-windows/releases/download/1.1.0/nvm-noinstall.zip";
+
+# Following the manual installation guide - https://github.com/coreybutler/nvm-windows/wiki#manual-installation
+
+Install-ChocolateyZipPackage -PackageName "nvm" -Url  $ZipUrl -UnzipLocation $AppDataNVM;
+
+New-Item $NvmSettings -type file -force -value `
+ $("root: $AppDataNVM`r`npath: $NodeJsInstall`r`narch: $OsBits`r`nproxy: none");
+
+Install-ChocolateyEnvironmentVariable -VariableName "NVM_HOME" -VariableValue "$AppDataNVM" -VariableType User;
+Install-ChocolateyEnvironmentVariable -VariableName "NVM_SYMLINK" -VariableValue "$NodeJsInstall" -VariableType Machine;
+
+Install-ChocolateyPath -PathToInstall  "%NVM_HOME%;%NVM_SYMLINK%" -PathType Machine;
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,17 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$uninstalled = $false;
+$AppDataNVM = "$env:APPDATA\nvm";
+
+nvm off;
+Remove-Item $AppDataNVM -Force -Recurse;
+Install-ChocolateyEnvironmentVariable -VariableName "NVM_HOME" -VariableValue $null -VariableType User
+Install-ChocolateyEnvironmentVariable -VariableName "NVM_SYMLINK" -VariableValue $null -VariableType Machine
+# TODO - remove "%NVM_HOME%;%NVM_SYMLINK%"
+# Uninstall-ChocolateyPath "%NVM_HOME%;%NVM_SYMLINK%" -PathType Machine;
+
+# Uninstall-ChocolateyEnvironmentVariable -VariableName "NVM_HOME" -VariableType User;
+# Uninstall-ChocolateyEnvironmentVariable -VariableName "NVM_SYMLINK" -VariableType Machine;
+
+$uninstalled = $true
